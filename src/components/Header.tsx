5f376be974335e148ac63be1e23089fa2b1dbf7b
Marco Di Toro <marcorrditoro@gmail.com>
2026-09-25 09:05:21 -0300

    remove pricing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,6 @@ const PRIMARY_LINKS: NavLinkDef[] = [
 
 const SECONDARY_LINKS: NavLinkDef[] = [
   { href: "/infos", label: "material teórico", exactMatch: true },
-  { href: "/pricing", label: "preços", exactMatch: true },
 ];
 
 //  NavLink component
